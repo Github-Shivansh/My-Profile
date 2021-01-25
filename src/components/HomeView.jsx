83f98658cd51814de09655276706d861d9633428
Shivansh1980 <shivanshshrivastava1980@gmail.com>
2021-01-25 20:42:43 +0530

    updated portfollio

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -1,17 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react'
+import { animateText} from '../animations'
 
 export class HomeView extends Component {
+    componentDidMount() {
+        animateText("introduction","backInDown",5,5,true);
+        animateText("name", "bounce", 3, 5, 15, true);
+    }
     render() {
         return (
             <div className="HomeView">
-                <div className="ShivanshImage">
-                    <img src={process.env.PUBLIC_URL + "/static/images/shivansh-2.png"} alt="shivansh" />
-                    <h2 id="name" align="center">Shivansh Shrivastava</h2>
+                <div className="firstDiv">
+                    <div className="ShivanshImage">
+                        <img src={process.env.PUBLIC_URL + "/static/images/shivansh-2.png"} alt="shivansh" />
+                        <h2 id="name" className="name" align="center">Shivansh Shrivastava</h2>
+                    </div>
+                    <p className="introduction" align="center">
+                            Hello, I am Shivansh Shrivastava.
+                    </p>
                 </div>
-                <p className="introduction" align="center">
-                    <span className="text-info1">Hello, I am Shivansh <br/>Shrivastava.</span> <br />
-                    <span className="text-info1">I am a student of B.Tech 3rd Year.</span>
-                </p>
+                <div className="aboutMe" data-aos="animate-aboutme-text">
+                    <h1>About Me</h1>
+                    <p className="aboutMeText">
+                        I’m proud of my ability to persevere and overcome challenges. 
+                        I have the 2 years of programming experience you’re looking for
+                        , a track record of successful projects, and proven expertise 
+                        in agile development processes. At the same time, I have developed 
+                        my communication skills from working directly with my friends,
+                        which means I am well prepared to work on high-profile, cross-department projects. I have the experience to start contributing from day one and I am truly excited about the prospect of getting started
+                    </p>
+                </div>
             </div>
         )
     }
--- src/components/ProjectView.jsx
+++ src/components/ProjectView.jsx
@@ -1,20 +1,27 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import AOS from 'aos';
+import { animateText } from '../animations'
 export class ProjectView extends Component {
+    componentDidMount() {
+        AOS.init();
+    }
+    componentDidUpdate() {
+        AOS.init()
+    }
     render() {
         return (
-            <div className="project">
+            <div data-aos="fade-up" data-aos-duration="1500" className="project">
                 <div className="project-header">
                     <h3>{this.props.project.name}</h3>
                     <br/>
                     <img src={this.props.project.imageurl} alt={this.props.project.name+"_pic"} />
                 </div>
                 <div className="project-description">
-                    <p>
-                        <span className="descriptions">Technology Used :</span> {this.props.project.tech_used}
+                    <p className="descriptions">
+                        Technology Used : {this.props.project.tech_used}
                     </p>
-                    <p>
-                        <span className="descriptions">Description : </span> {this.props.project.description}
+                    <p className="descriptions">
+                       Description : {this.props.project.description}
                     </p>
                 </div>
             </div>
