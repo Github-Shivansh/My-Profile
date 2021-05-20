9d50181e619231736e8a1ae3d89cf3a88bdd60b5
Shivansh1980 <49563393+Shivansh1980@users.noreply.github.com>
2021-05-20 14:55:06 +0530

    contact form updated

--- src/components/ContactView.jsx
+++ src/components/ContactView.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { FaFacebookMessenger, FaPaypal } from 'react-icons/fa'
+import { FaFacebookMessenger, FaLinkedin, FaGithub } from 'react-icons/fa'
+import axios from 'axios'
+import $ from 'jquery'
 export class ContactView extends Component {
     state = {
         name: '',
@@ -8,14 +10,38 @@ export class ContactView extends Component {
         message: '',
     }
     data = {}
-    buttonSubmit() {
-        alert('Hello')
+    componentDidMount() {
+        $('#contact_form').click(function (e) {
+            e.preventDefault();
+            var formData = new FormData(this);
+            // if (localStorage.getItem('user_email')) {
+            //     alert('you cannot send message multiple times');
+            //     return false;
+            // }
+            localStorage.setItem('user_email', $('#user_email').val());
+            axios({
+                method: 'POST',
+                url: 'http://127.0.0.1:8000/sendmail/',
+                data: formData,
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }).then(function (response) {
+                if (response.error) alert(response.error_msg);
+                else alert(response.message);
+            })
+            e.preventDefault();
+        })
     }
 
     render() {
         return (
-            <div id="Contact" data-aos="zoom-in" data-aos-duration="2000" className="ContactForm">
+            <form id="contact_form" data-aos="zoom-in" data-aos-duration="2000" className="ContactForm">
                 <h1>Contact Me</h1>
+                <div className="contact_icon_container">
+                    <FaLinkedin size="1.5em" className="contact_icon contact_linkedin" color="blue" />
+                    <FaGithub size="1.5em" className="contact_icon contact_github"/>
+                </div>
                 <p>For any extra information. Send me message </p>
                 <div>
 
@@ -23,36 +49,33 @@ export class ContactView extends Component {
                 <div className="InputFields">
                     <input
                         type="text"
+                        name="name"
                         value={this.state.name}
                         onChange={e => this.setState({ name: e.target.value })}
                         placeholder="Name"
                     />
                     <input
+                        id='user_email'
                         type="email"
+                        name="from"
                         value={this.state.email}
                         onChange={e => this.setState({ email: e.target.value })}
                         placeholder="Email"
                     />
-                    <input
-                        type="text"
-                        value={this.state.subject}
-                        onChange={e => this.setState({ subject: e.target.value })}
-                        placeholder="Subject"
-                    />
+
                     <textarea
+                        id="contact_form_textarea"
                         type="text"
+                        name="message"
                         value={this.state.message}
                         onChange={e => this.setState({ message: e.target.value })}
                         placeholder="Message"
                     />
-                    <button class="btn" onClick={this.buttonSubmit}>
+                    <button class="btn" type="submit">
                         <FaFacebookMessenger size="1em" color="white" />  Send Message
                     </button>
-                    <button class="btn" onClick={this.buttonSubmit}>
-                        <FaPaypal size="1em" color="white" />  Pay Now
-                    </button>
                 </div>
-            </div>
+            </form>
         )
     }
 }
